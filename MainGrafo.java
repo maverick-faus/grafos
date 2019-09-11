236645a2a1715002213f8369e27ce52760c64562
Jose Faustinos <jfaustinos19@gmail.com>
2019-09-11 12:44:21 -0500

    Archivos Practica 1

--- MainGrafo.java
+++ MainGrafo.java
@@ -1,22 +1,20 @@
 import java.util.HashMap;
 
-public class Hola {
+public class MainGrafo {
     public static void main(String args[]) {
         Grafo g1 = new Grafo();
-
         // aristas,nodos,dirigido,autociclo
-        /*g1.ErdosRenyi(500, 50, false, false);
-        g1 = new Grafo();
+        g1.ErdosRenyi(10, 5, true, true);
 
         // nodos,proba,dirigido,autociclo
-        g1.Gilbert(50, (float) 0.5, false, false);
+        g1.Gilbert(5, (float) 0.5, true, true);
         g1 = new Grafo();
-*/
+
         // nodos,distancia,dirigido,autociclo
-        g1.Geo(1000, .1, false, false);
+        g1.Geo(5, .1, true, true);
         g1 = new Grafo();
 
         // nodos,factor,dirigido,autociclo
-        g1.BarabasiAlbert(1000, 5, false, false);
+        g1.BarabasiAlbert(5, 5, true, true);
     }
 }
